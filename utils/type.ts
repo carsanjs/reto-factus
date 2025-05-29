b0167a57394603ab3ce8773d9127d264da88ecaa
Carlos Sanchez Carrascal <carlosstqm2225@gmail.com>
2025-05-29 14:10:40 -0500

    add types notas

--- utils/type.ts
+++ utils/type.ts
@@ -91,3 +91,151 @@ interface FactusResponse {
     details?: any;
   };
 }
+
+////// PAGINATION PAGES ///////
+interface Pagination {
+  total: number;
+  per_page: number;
+  current_page: number;
+  last_page: number;
+  from: number;
+  to: number;
+  links: [
+    {
+      url?: null;
+      label: string;
+      page?: null;
+      active: false;
+    }
+  ];
+}
+
+///// OBNTENER NOTA CREDITOS
+interface NotaCreditoAll {
+  id: number;
+  api_client_name: string;
+  reference_code: string;
+  number: string;
+  identification: string;
+  graphic_representation_name: string;
+  company?: string;
+  trade_name?: null;
+  names: string;
+  email?: null;
+  total: string;
+  status: number;
+  errors: [];
+  send_email: number;
+  created_at: string;
+}
+
+//// CREATED NOTA DE CREDITO
+interface CreateNotaCredito {
+  company: {
+    url_logo: string;
+    nit: string;
+    dv: string;
+    company: string;
+    name: string;
+    registration_code: string;
+    economic_activity: string;
+    phone: string;
+    email: string;
+    direction: string;
+    municipality: string;
+  };
+  customer: {
+    identification: string;
+    dv?: null;
+    graphic_representation_name: string;
+    trade_name?: string;
+    company?: string;
+    names: string;
+    address: string;
+    email: string;
+    phone: string;
+    legal_organization: {
+      id: number;
+      code: string;
+      name: string;
+    };
+    tribute: {
+      id: number;
+      code: string;
+      name: string;
+    };
+    municipality: {
+      id: number;
+      code: string;
+      name: string;
+    };
+  };
+  credit_note: {
+    id: number;
+    number: string;
+    reference_code: string;
+    status: number;
+    send_email: number;
+    qr: string;
+    cude: string;
+    validated: string;
+    discount_rate: string;
+    discount: string;
+    gross_value: string;
+    taxable_amount: string;
+    tax_amount: string;
+    total: string;
+    observation?: null;
+    errors: [];
+    created_at: string;
+    qr_image: string;
+    bill_id: number;
+    cufe: string;
+    number_bill: string;
+    payment_method: {
+      name: string;
+      code: string;
+    };
+    customization_id: {
+      code: string;
+      name: string;
+    };
+    correction_concept: {
+      code: string;
+      name: string;
+    };
+  };
+  items: [
+    {
+      code_reference: string;
+      name: string;
+      quantity: number;
+      discount_rate: string;
+      discount: string;
+      gross_value: string;
+      tax_rate: string;
+      taxable_amount: string;
+      tax_amount: string;
+      price: string;
+      is_excluded: number;
+      unit_measure: {
+        id: number;
+        code: string;
+        name: string;
+      };
+      standard_code: {
+        id: number;
+        code: string;
+        name: string;
+      };
+      tribute: {
+        id: number;
+        code: string;
+        name: string;
+      };
+      total: number;
+      withholding_taxes: [];
+    }
+  ];
+  withholding_taxes: [];
+}
